72805d2eb6bb4c0069b762c5fedd469ccc24eb39
jo.robson <jose.robson@rerum.com.br>
2020-08-05 16:05:11 -0300

    ajustes nas imagens

---
+++ build/precache-manifest.308af62a1f208d05b3c3c96d619ae852.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168a06a4c1ccdb822f0ce82529a4cbb0",
+    "url": "/simulador-cesta-basica/index.html"
+  },
+  {
+    "revision": "5a631d641cd92ec2dae8",
+    "url": "/simulador-cesta-basica/static/css/2.ccab356c.chunk.css"
+  },
+  {
+    "revision": "778d6077feaa51144a32",
+    "url": "/simulador-cesta-basica/static/css/main.61a7015a.chunk.css"
+  },
+  {
+    "revision": "5a631d641cd92ec2dae8",
+    "url": "/simulador-cesta-basica/static/js/2.d97f3e1a.chunk.js"
+  },
+  {
+    "revision": "4d6ab1395afd48c38ab429e9c82a1cee",
+    "url": "/simulador-cesta-basica/static/js/2.d97f3e1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778d6077feaa51144a32",
+    "url": "/simulador-cesta-basica/static/js/main.62ea7a9b.chunk.js"
+  },
+  {
+    "revision": "ea5e311ac5f02655f6b7",
+    "url": "/simulador-cesta-basica/static/js/runtime-main.f4058d54.js"
+  }
+]);
